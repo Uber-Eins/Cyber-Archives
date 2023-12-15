a998aef18c28ec594b7c913b09ef28b44423433b
Uber-Eins <111227214+Uber-Eins@users.noreply.github.com>
2023-12-15 14:00:28 +0800

    Update re.js

--- Surge/Module/re.js
+++ Surge/Module/re.js
@@ -1,7 +1,13 @@
 let url = $request.url;
+let headers = $request.headers;
+
+url.hostname = $argument;
+
+headers["Host"] = $argument;
+
 let urlQuery = new URLSearchParams(url.search);
 urlQuery.set("os", "upos");
 urlQuery.delete("mcdnid");
 url.search = urlQuery.toString();
-url.hostname = $argument;
-$done(url);
+
+$done(url,headers);
